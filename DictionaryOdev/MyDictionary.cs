7a997e685d2f8966f4cb4149eac4e386dcca69f8
gokhankoyu <gokhankoyu11@gmail.com>
2021-01-23 18:03:40 +0300

    Add project files.

---
+++ DictionaryOdev/MyDictionary.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DictionaryOdev
+{
+    class MyDictionary<TKey, TValue>
+    {
+        TKey[] keyArray;
+        TValue[] valueArray;
+
+        public MyDictionary()
+        {
+            keyArray = new TKey[0];
+            valueArray = new TValue[0];
+        }
+
+        public void Add(TKey itemKey, TValue itemValue)
+        {
+
+            TKey[] tempKeyArray = keyArray;
+            TValue[] tempValueArray = valueArray;
+
+            keyArray = new TKey[keyArray.Length + 1];
+            valueArray = new TValue[valueArray.Length + 1];
+
+            for (int i = 0; i < tempValueArray.Length; i++)
+            {
+                keyArray[i] = tempKeyArray[i];
+                valueArray[i] = tempValueArray[i];
+            }
+
+            keyArray[keyArray.Length - 1] = itemKey;
+            valueArray[valueArray.Length - 1] = itemValue;
+
+        }
+        public void printAll()
+        {
+            for (int i = 0; i < keyArray.Length; i++)
+            {
+                Console.WriteLine(keyArray[i] + " Key , Value : " + valueArray[i]);
+                Console.WriteLine("-----------------------------------------------------------");
+                Console.WriteLine();
+            }
+        }
+
+        public TKey[] Items1
+        {
+            get { return keyArray; }
+        }
+
+        public TValue[] Items2
+        {
+            get { return valueArray; }
+        }
+
+        public void Count()
+        { 
+        
+        }
+        //public void Cantains()
+        //{
+        //    get { return }
+
+        //}
+    }
+}
---
+++ DictionaryOdev/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace DictionaryOdev
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //IDictionary<int, string> musteriler = new Dictionary<int, string>();
+            //musteriler.Add(2,"gercek1");
+            //musteriler.Add(4, "gercek2");
+            //musteriler.Add(12, "tuzel1");
+            //musteriler.Add(14, "tuzel2");
+
+
+            //foreach (KeyValuePair<int, string> musteri in musteriler)
+            //{
+            //    Console.WriteLine("{0}, {1}", musteri.Key, musteri.Value );
+            //}
+
+            MyDictionary<string, string> idOgrenci = new MyDictionary<string, string>();
+
+            idOgrenci.Add("125", "Ayşe");
+            idOgrenci.Add("390","Gökhan");
+
+
+            idOgrenci.printAll();
+
+            for (int i = 0; i < idOgrenci.Count; i++) // bununla ilgili metodu eklemeyi öğrenmem lazım.
+            {
+            Console.WriteLine(idOgrenci.Items1[0], idOgrenci.Items2[0]);
+            }
+
+        }
+
+
+    }
+}
